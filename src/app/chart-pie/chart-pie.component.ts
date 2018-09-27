71ce8d32f4a71c7716148648efaea96732a76d3d
SAMIR HASSAN AYOUB <samir.ayoub@SPOI010114179.local>
2018-09-27 13:38:51 -0300

    trabalhando cores no chart pie

--- src/app/chart-pie/chart-pie.component.ts
+++ src/app/chart-pie/chart-pie.component.ts
@@ -8,116 +8,116 @@ import * as d3 from 'd3';
   styleUrls: ['./chart-pie.component.css']
 })
 export class ChartPieComponent implements OnInit {
-
   arc;
-  path;
-  arcc;
-  color;
   data;
   g;
-  height;
-  label;
-  margin = { top: 32, right: 16, bottom: 56, left: 64 };
   pie;
-  svg;
-  width;
+  color = ['#f44842', '#f4b841', '#f4f141', '#46f441', '#41f4d9', '#4941f4', '#f441d6'];
+  margin = { top: 32, right: 16, bottom: 56, left: 64 };
   radius;
+  svg;
+  label;
+  width;
+  height;
+  pathArea;
+  path;
 
-  constructor() { }
 
-  ngOnInit() {
-    this.data = [
-      {name: "USA", valor: 60},
-      {name: "UK", valor: 20},
-      {name: "Canada", valor: 30},
-      {name: "Maxico", valor: 15},
-      {name: "Japan", valor: 10},
-    ];
-
-    this.initChart();
+  constructor() {
 
   }
 
-  initChart() {
+  ngOnInit() {
+    this.data = [
+      { city: 'Deesa', beautifulRatio: 30 },
+      { city: 'Patan', beautifulRatio: 20 },
+      { city: 'Ahmedabad', beautifulRatio: 15 },
+      { city: 'Bhabhar', beautifulRatio: 35 },
+      { city: 'BHakhasar', beautifulRatio: 50 }];
+
     this.width = document.getElementById('chartline').offsetWidth - this.margin.left - this.margin.right;
     this.height = 300 - this.margin.top - this.margin.bottom;
     this.radius = Math.min(this.width, this.height) / 2;
 
-    this.color = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888"]);
-
-
     this.path = d3.arc()
-      .outerRadius(this.radius - 10)
-      .innerRadius(0);
-
-    this.label = d3.arc()
-      .outerRadius(this.radius - 40)
-      .innerRadius(this.radius - 40);
-
-    this.pie = d3.pie().sort(null).value(function(d: any) { return d.valor; });
-
+      .outerRadius(this.radius - 20)
+      .innerRadius(0)
+      .cornerRadius(5);
 
     this.svg = d3.select('.svg').append('svg')
       .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .attr('height', this.height + this.margin.top + this.margin.bottom);
 
     this.g = this.svg.append('g')
       .attr('transform', 'translate(' + (this.width / 2) + ',' + (this.height / 2) + ')');
 
 
+    this.pie = d3.pie().sort(null).value(function (d: any) { return d.beautifulRatio; });
 
-    this.arc = this.g.selectAll(".arc")
+    this.arc = this.g.selectAll('.arc')
       .data(this.pie(this.data))
-      .enter().append("g")
-      .attr("class", "arc");
+      .enter().append('g')
+      .classed('arc', true);
 
-    this.arc.append("path")
-      .attr("d", this.path)
-      .attr("fill", function(d) { return this.color(d.data)});
+    this.label = d3.arc()
+      .outerRadius(this.radius - 40)
+      .innerRadius(this.radius - 40);
 
-    this.arc.append("text")
-      .attr("transform", function (d) { return "translate(" + this.label.centroid(d) + ")"; })
-      .attr("dy", "0.35em")
-      .text(function (d) { return d.name; });
+
+    this.drawPath();
+
+    this.drawLabels();
   }
 
+  drawPath() {
+    const color = this.color;
+    this.pathArea = this.arc.append('path')
+      .attr('d', this.path)
+      .attr('id', function (d, i) {
+        return 'arc-' + i;
+      })
+      .attr('style', 'fill-opacity: 0.85;')
+      .attr('fill', function (d, i) {
+        return color[i];
+      })
+      .attr('data', function (d) {
+        d.data['percentage'] = (d.endAngle - d.startAngle) / (2 * Math.PI) * 100;
+        return JSON.stringify(d.data);
+      });
+
+  }
+
+  drawLabels() {
 
 
-  // this.g = this.svg.append('g').attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+    this.arc.append('text')
+        .data(this.data)
+        .attr('transform', function(d) {
+          let centroid = this.arc.centroid(d);
+          console.log(centroid);
+            d3.select(this)
+            .attr('x', centroid[0])
+            .attr('y', centroid[1])
+            .attr('dy', '0.33em')
+            .text(d.label);
+        })
+        .attr('dy', '0.35em')
+        .text(function(d) {
+            return d.beautifulRatio;
+        });
+  }
 
-  // this.g.append('g')
-  //   .selectAll(".arc")
-  //   .data(this.pie(this.data))
-  //   .enter().append("g")
-  //   .attr("class", "arc");
-
-  // this.g.append("path")
-  //   .attr("d", this.arc)
-  //   .style("fill", function (d) { return this.color(d.data); });
-
-  // this.g.append("text")
-  //   .attr("transform", function (d) { return "translate(" + this.labelArc.centroid(d) + ")"; })
-  //   .attr("dy", ".35em")
-  //   .text(function (d) { return d.data; });
-
-  // this.xScale = d3.scalePoint()
-  // .domain(this.xScaleData.map(categorie => {return categorie}))
-  //     .range([0, this.width]);
-
-  // this.yScale = d3.scaleLinear()
-  // .domain([0, this.getCategorieMaxValue()])
-  // .range([this.height, 0]);
-
-  // this.line = d3Shape.line()
-  // .curve(d3.curveLinear)
-  // .x( (d: any) => this.xScale(d.month) )
-  // .y( (d: any) => this.yScale(d.sales) );
-
-  // this.svg = d3.select('.svg').append('svg')
-  //   .attr('width', this.width + this.margin.left + this.margin.right)
-  //   .attr('height', this.height + this.margin.top + this.margin.bottom)
-
-  // this.g = this.svg.append('g').attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 }
 
+// d3.select('g')
+// 	.selectAll('text')
+// 	.data(arcData)
+// 	.enter()
+// 	.append('text')
+// 	.each(function(d) {
+// 		var centroid = arcGenerator.centroid(d);
+// 		d3.select(this)
+// 			.attr('x', centroid[0])
+// 			.attr('y', centroid[1])
+// 			.attr('dy', '0.33em')
+// 			.text(d.label);
